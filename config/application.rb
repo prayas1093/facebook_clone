8476fa68b1570a5c6005dcaf07e4567d2623a9e6
Prayas.Arora <prayas@PrayasAroras-MacBook-Air.local>
2020-01-31 17:45:08 +0530

    Mailer settings for heroku production

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,21 @@ module FacebookClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.action_mailer.perform_caching = false
+    if  Rails.env=="development"
+      config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    else
+      config.action_mailer.default_url_options = { host: 'prayas-fb-clone.herokuapp.com'}
+    end
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        :user_name => '9cfb59a077ed12',
+        :password => '03f99c34d712db',
+        :address => 'smtp.mailtrap.io',
+        :domain => 'smtp.mailtrap.io',
+        :port => '2525',
+        :authentication => :cram_md5
+      }  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
